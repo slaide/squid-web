4464174390d51ef8469cdbd4df42e5565015cf0c
Patrick Hennig <patrick_hennig@fastmail.com>
2023-11-17 17:29:42 +0100

    improve logic in dynamic images to properly center them on load

--- components/dynamic-image/logic.js
+++ components/dynamic-image/logic.js
@@ -1,13 +1,45 @@
+function center_image(element){
+    if(element.classList.contains("dynamic-image")){
+        element=element.parentNode;
+    }
+
+    let img_height=element.children[0].clientHeight
+    let img_width=element.children[0].clientWidth
+
+    let half_img_height=img_height/2
+    let half_img_width=img_width/2
+
+    // calculate scale so that image fits into container
+    let img_scale_to_fit=Math.min(element.clientHeight/img_height,element.clientWidth/img_width)*0.95
+    element.image_scale=img_scale_to_fit
+
+    let scaled_img_height=img_scale_to_fit*img_height
+    let scaled_img_width=img_scale_to_fit*img_width
+
+    // then calculate offset to center image within container
+    let base_offset_x=(element.clientWidth-scaled_img_width)/2
+    let base_offset_y=(element.clientHeight-scaled_img_height)/2
+
+    element.image_offset_left=base_offset_x-half_img_width+scaled_img_width/2
+    element.image_offset_top=base_offset_y-half_img_height+scaled_img_height/2
+
+    element.children[0].style.setProperty("--left",element.image_offset_left+"px")
+    element.children[0].style.setProperty("--top",element.image_offset_top+"px")
+    element.children[0].style.setProperty("--scale",element.image_scale)
+}
+
 document.addEventListener("DOMContentLoaded",function(){
     for(element of document.getElementsByClassName("dynamic-image-display")){
         let image_container_element=document.createElement("div")
         image_container_element.classList.add("dynamic-image-container")
         element.appendChild(image_container_element)
 
+        // initialize these values to _something_
         image_container_element.image_offset_top=0;
         image_container_element.image_offset_left=0;
         image_container_element.image_scale=1;
 
+        image_container_element.addEventListener("dblclick",(event)=>center_image(event.currentTarget))
         image_container_element.addEventListener("mousedown",function(event){
             event.preventDefault()
 
@@ -83,70 +115,20 @@ document.addEventListener("DOMContentLoaded",function(){
             image_element.setAttribute("src",img_src)
         }
 
-        let observer = new IntersectionObserver((entries, observer) => {
-        entries.forEach(entry => {
-            if (entry.isIntersecting) {
-                // get img size
-                let img_height=entry.target.clientHeight
-                let img_width=entry.target.clientWidth
-
-                // get container size
-                let container_height=entry.target.parentNode.clientHeight
-                let container_width=entry.target.parentNode.clientWidth
-
-                // calculate image scale to display in full
-                let height_ratio=container_height/img_height
-                let width_ratio=container_width/img_width
-
-                // reduce scale slightly for some nicer visual presentation (and to make it clear that the whole image is in view)
-                let scale=Math.min(height_ratio,width_ratio)*0.95
-
-                // calculate offsets to center image
-                let offset_left=(container_width-img_width)/2
-                let offset_top=(container_height-img_height)/2
-
-                // save new scale and offsets
-                entry.target.parentNode.image_scale=scale
-                entry.target.parentNode.image_offset_left=offset_left
-                entry.target.parentNode.image_offset_top=offset_top
-
-                // apply new scale and offsets
-                entry.target.parentNode.children[0].style.setProperty("--scale",entry.target.parentNode.image_scale)
-                entry.target.parentNode.children[0].style.setProperty("--left",entry.target.parentNode.image_offset_left+"px")
-                entry.target.parentNode.children[0].style.setProperty("--top",entry.target.parentNode.image_offset_top+"px")
-        
-                // stop observing
-                observer.unobserve(entry.target);
-
-                if(!isFinite(entry.target.parentNode.image_scale)){
-                    let interval_timer=setInterval(function(){
-                        let img_height=entry.target.clientHeight
-                        let img_width=entry.target.clientWidth
-            
-                        // get container size
-                        let container_height=entry.target.parentNode.clientHeight
-                        let container_width=entry.target.parentNode.clientWidth
-            
-                        // calculate image scale to display in full
-                        let height_ratio=container_height/img_height
-                        let width_ratio=container_width/img_width
-            
-                        // reduce scale slightly for some nicer visual presentation (and to make it clear that the whole image is in view)
-                        let scale=Math.min(height_ratio,width_ratio)*0.95
-
-                        entry.target.parentNode.image_scale=scale
-
-                        if(isFinite(entry.target.parentNode.image_scale)){
-                            entry.target.parentNode.children[0].style.setProperty("--scale",entry.target.parentNode.image_scale)
-                            clearInterval(interval_timer)
-                        }
-                    },100)
+        // once element is loaded, wait for first draw, then center it
+        // (workaround for multiple browsers where image size is not determined on load, but on display)
+        image_element.addEventListener("load",(event)=>{
+            let element=event.currentTarget
+            let intersection_observer=new IntersectionObserver((entries)=>{
+                for(let entry of entries){
+                    if(entry.isIntersecting){
+                        center_image(element)
+                        intersection_observer.disconnect()
+                    }
                 }
-            }
-        });
-        }, { threshold: 0.01 }); // Execute callback when at least 1% of the target is visible
-    
-        observer.observe(image_element);
+            })
+            intersection_observer.observe(element)
+        })
 
         image_container_element.appendChild(image_element)
     }
