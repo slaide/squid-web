eb017470aa4d6c17e560996e993c884b027e904e
Patrick Hennig <patrick_hennig@fastmail.com>
2023-11-21 16:12:23 +0100

    layout improvements and initial server communication for config verification

--- server.py
+++ server.py
@@ -1,16 +1,77 @@
 #! /usr/bin/env python3
 
-from flask import Flask, send_file, abort
+from flask import Flask, send_file, abort, request
 from PIL import Image
 import os
 import io
 import numpy as np
 import traceback
 
+from pathlib import Path
+
 app = Flask(__name__)
 
 def print_exception(e):
     traceback.print_exception(type(e), e, e.__traceback__)
+
+def expect_config():
+    if not request.is_json:
+        return "request does not indicate json content", 400
+
+    try:
+        data=request.get_json()
+    except:
+        return "request body is not valid json", 400
+    
+    return data
+
+@app.route('/api/fullPathName', methods=['POST'])
+def fullPathName():
+    data=expect_config()
+
+    full_output_path=str(Path(data['base_path'])/data['project_name']/data['plate_name'])
+
+    # return json object
+    return {
+        "full_output_path": full_output_path
+    }
+
+@app.route('/api/requiredStorage', methods=['POST'])
+def grequiredStorageetit():
+    data=expect_config()
+
+    # get number of bytes per pixel
+    pixel_depth_value=data['pixel_depth']['handle']
+    if(pixel_depth_value=='mono8'):
+        num_bytes_per_pixel=1
+    elif(pixel_depth_value=='mono12'):
+        num_bytes_per_pixel=2
+    else:
+        raise ValueError(f"pixel_depth must be mono8 or mono12 but is instead '{pixel_depth_value}'")
+    
+    # get number of positions per well
+    num_pos_per_well=data['grid']['num_x']*data['grid']['num_y']*data['grid']['num_z']*data['grid']['num_t']
+
+    # get number of selected channels
+    num_channels=1 # TODO
+
+    # get number of selected wells
+    num_wells_selected=0
+    for row in data['well_selection']:
+        for col in row:
+            if col:
+                num_wells_selected+=1
+
+    # finally calculate max required storage
+    num_images=num_wells_selected*num_pos_per_well*num_channels
+    
+    pixel_per_image=2500*2500 
+    max_required_storage=num_images*num_bytes_per_pixel*pixel_per_image
+
+    # return json object
+    return {
+        "max_required_storage": max_required_storage
+    }    
 
 @app.route('/<path:filename>')
 def get_file(filename):
